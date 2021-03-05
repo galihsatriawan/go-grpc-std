e9e53d6b229fae689098cddad526f2116456866f
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2021-03-05 14:02:41 +0700

    Create format response and helper for formatting

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/galihsatriawan/go-grpc-std
 
 go 1.15
+
+require (
+	github.com/galihsatriawan/go-catch v0.0.1
+	google.golang.org/protobuf v1.25.0
+)
---
+++ helper/helper.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"github.com/galihsatriawan/go-catch"
+	"github.com/galihsatriawan/go-grpc-std/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+func GrpcResponse(code int, message string, status string, data protoreflect.ProtoMessage) *proto.GRPCResponse {
+	meta := &proto.Meta{
+		Code:    int32(code),
+		Message: message,
+		Status:  status,
+	}
+	anyData, err := anypb.New(data)
+	catch.Catch(nil, err, "Fail when try to cast to any type")
+	response := proto.GRPCResponse{
+		Meta: meta,
+		Data: anyData,
+	}
+	return &response
+}
