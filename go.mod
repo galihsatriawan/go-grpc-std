6d8573231bf60aa2f19c5f0c7b44403815d1304c
Galih Anggi Satriawan <satriawan.galih99@gmail.com>
2021-03-20 15:12:23 +0700

    Update package

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/galihsatriawan/go-grpc-std
 go 1.15
 
 require (
-	github.com/galihsatriawan/go-catch v0.0.1
-	google.golang.org/protobuf v1.25.0
+	github.com/galihsatriawan/go-catch v0.0.0-20210315070632-f96e481b4ffa
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.26.0
 )
--- helper.go
+++ helper.go
@@ -2,22 +2,28 @@ package std
 
 import (
 	"github.com/galihsatriawan/go-catch"
-	"github.com/galihsatriawan/go-grpc-std/proto"
+	pb "github.com/galihsatriawan/go-grpc-std/proto"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
-func GrpcResponse(code int, message string, status string, data protoreflect.ProtoMessage) *proto.GRPCResponse {
-	meta := &proto.Meta{
+func GrpcResponse(code int, message string, status string, data protoreflect.ProtoMessage) *pb.GRPCResponse {
+	meta := &pb.Meta{
 		Code:    int32(code),
 		Message: message,
 		Status:  status,
 	}
 	anyData, err := anypb.New(data)
 	catch.Catch(nil, err, "Fail when try to cast to any type")
-	response := proto.GRPCResponse{
+	response := pb.GRPCResponse{
 		Meta: meta,
 		Data: anyData,
 	}
+
 	return &response
+}
+func MappingResponse(src *anypb.Any, dst protoreflect.ProtoMessage) {
+	err := anypb.UnmarshalTo(src, dst, proto.UnmarshalOptions{})
+	catch.Catch(nil, err, "Fail when try to decode response")
 }
